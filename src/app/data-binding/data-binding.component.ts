e0fcc5655e8d26afe31bdd71f255754af0d6483d
Sara Gomes <sarah.gomes80@hotmail.com>
2023-05-04 12:16:04 -0300

    section 4 [Angular]: Template Data Binding - Property Binding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -11,6 +11,10 @@ export class DataBindingComponent implements OnInit {
   public idade: number = 29;
   public maisUm: number = 1;
 
+  public checkedDisabled: boolean = true;
+  public imgSrc: string = "https://images.unsplash.com/photo-1550745165-9bc0b252726f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+  public imgTitle: string = "Property Binding";
+  
   constructor() {}
 
   ngOnInit(): void {}
