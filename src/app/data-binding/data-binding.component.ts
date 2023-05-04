faabc2e613d0c877a9c2ead090e0cb1570bb6e43
Sara Gomes <sarah.gomes80@hotmail.com>
2023-05-04 13:44:28 -0300

    section 4 [Angular]: Template Data Binding - Event Binding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -14,9 +14,28 @@ export class DataBindingComponent implements OnInit {
   public checkedDisabled: boolean = true;
   public imgSrc: string = "https://images.unsplash.com/photo-1550745165-9bc0b252726f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
   public imgTitle: string = "Property Binding";
+
+  public position: {x: number, y: number} = {x: 0, y: 0};
   
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {  }
+
+  public alertaInfoSemParametro(){
+    alert("alertaInfoSemParametro")
+  } 
+  public alertaInfoComParametro(valor: string){
+    alert(valor)
+  } 
+  public alertaInfoMouseEvent(valor: MouseEvent){
+    console.log(valor)
+  } 
+
+  public mouseMoveTeste(valor: MouseEvent){
+    this.position.x = valor.offsetX
+    this.position.y = valor.offsetY
+  } 
+   
+
  
 }
