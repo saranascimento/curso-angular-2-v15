d1555720c93e80c964ff737aa2d3d65f9cf177be
Sara Gomes <sarah.gomes80@hotmail.com>
2023-05-05 14:54:03 -0300

    section 5 [Angular]: Diretivas - ngStyle

--- src/app/diretivas-atributos/diretivas-atributos.component.ts
+++ src/app/diretivas-atributos/diretivas-atributos.component.ts
@@ -8,11 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class DiretivasAtributosComponent implements OnInit {
 
   public valor: boolean = true;
+  public heightPx: string = "20px";
+  public backgroundColor: string = "red";
 
   constructor() {}
   ngOnInit(): void {
     setInterval(() => {
       this.valor = !this.valor;
+
+      if (this.heightPx == "20px") {
+        this.heightPx = "50px";
+        this.backgroundColor = "blue";
+      } else {
+        this.heightPx = "20px";
+        this.backgroundColor = "red";
+      }
     }, 2000)
   }
 }
