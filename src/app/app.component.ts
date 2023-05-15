3ecb12923cc944f11df1975da8f42d4fc1dd2957
Sara Gomes <sarah.gomes80@hotmail.com>
2023-05-15 15:13:44 -0300

    section 5 [Angular]: Diretivas - ng-content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,17 @@ import {  Component, OnInit } from '@angular/core';
   selector: 'app-root',
   template: `
   <!-- <app-diretivas-estruturais></app-diretivas-estruturais> -->
-  <app-diretivas-atributos></app-diretivas-atributos>
+
+  <app-diretivas-atributos>
+    <h1>Aulas de Diretivas Atributos</h1>
+    <h3>Final da aula</h3>
+  </app-diretivas-atributos>
+
+  <app-diretivas-atributos>
+    <h1>Sara Gomes</h1>
+    <h3>Nay Torres</h3>
+  </app-diretivas-atributos>
+
   <router-outlet></router-outlet>
   `
 })
