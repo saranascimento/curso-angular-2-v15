64bce0203b5e870b485b3f28c33207ccbc0582da
Sara Gomes <sarah.gomes80@hotmail.com>
2023-05-25 11:36:46 -0300

    section 7 [angular] Comunicação entre componentes: @Output() - Parte 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,39 +2,26 @@ import {  Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <!-- <app-diretivas-estruturais></app-diretivas-estruturais> -->
+  <ng-template [ngIf]="getDados">
+    <h1>{{getDados.nome}}</h1>
+    <h1>{{getDados.idade}}</h1>
+  </ng-template>
 
-  <!-- <app-diretivas-atributos>
-    <h1>Aulas de Diretivas Atributos</h1>
-    <h3>Final da aula</h3>
-  </app-diretivas-atributos>
-
-  <app-diretivas-atributos>
-    <h1>Sara Gomes</h1>
-    <h3>Nay Torres</h3>
-  </app-diretivas-atributos> -->
-
-  <!-- <app-diretivas-atributos></app-diretivas-atributos> -->
-
-  <!-- <app-new-component></app-new-component> -->
+  <app-output (enviarDados)="setDados($event)"></app-output>
   
-  <!-- <app-input [contador]="addValue"></app-input>
-  <br>
-  <button (click)="add()">Add</button> -->
-  <app-output></app-output>
   <router-outlet></router-outlet>
   `
 })
 export class AppComponent implements OnInit {
 
-  public addValue: number = 0;
+  public getDados: { nome: string, idade: number } | undefined; 
 
   constructor() {}
 
   ngOnInit(): void {}
 
-  public add() {
-    this.addValue += 1;
+  public setDados(event: { nome: string, idade: number }) {
+    this.getDados = event;
   }
  
 }
