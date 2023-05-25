a598afe67f5907111bb01212bf8e00aebd6ba615
Sara Gomes <sarah.gomes80@hotmail.com>
2023-05-25 09:15:47 -0300

    section 7 [angular] Comunicação entre componentes: @Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,15 +16,25 @@ import {  Component, OnInit } from '@angular/core';
 
   <!-- <app-diretivas-atributos></app-diretivas-atributos> -->
 
-  <app-new-component></app-new-component>
+  <!-- <app-new-component></app-new-component> -->
+  
+  <app-input [contador]="addValue"></app-input>
+  <br>
+  <button (click)="add()">Add</button>
   <router-outlet></router-outlet>
   `
 })
 export class AppComponent implements OnInit {
 
+  public addValue: number = 0;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  public add() {
+    this.addValue += 1;
+  }
  
 }
 
