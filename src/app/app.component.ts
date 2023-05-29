a3e4c6f2529667ece92d8ec0aeb79fd78afb2fa6
Sara Gomes <sarah.gomes80@hotmail.com>
2023-05-29 15:34:06 -0300

    Service - Injeção de Dependencia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,7 @@ import {  Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <ng-template [ngIf]="getDados">
-    <h1>{{getDados.nome}}</h1>
-    <h1>{{getDados.idade}}</h1>
-  </ng-template>
-
-  <app-output (enviarDados)="setDados($event)"></app-output>
+  <app-food-list></app-food-list>
   
   <router-outlet></router-outlet>
   `
---
+++ src/app/services/food-list.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FoodListService {
+
+  private list: Array<string> = [
+    "X bacon", "Feijão", "Ovo"
+  ];
+
+  constructor() { }
+
+  public foodList() {
+    return this.list;
+  }
+}
