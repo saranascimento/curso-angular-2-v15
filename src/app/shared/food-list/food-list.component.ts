c921d2a785c0e81ad2df72a6f094a70dfef1c3d8
Sara Gomes <sarah.gomes80@hotmail.com>
2023-05-29 16:37:44 -0300

    Subscribe

--- src/app/shared/food-list/food-list.component.ts
+++ src/app/shared/food-list/food-list.component.ts
@@ -15,5 +15,9 @@ export class FoodListComponent implements OnInit {
 
   ngOnInit(): void {
     this.foodList = this.foodListService.foodList();
+
+    this.foodListService.emitEvent.subscribe(
+      res => alert(`Olha você add => ${res}`)
+    );
   }
 }
