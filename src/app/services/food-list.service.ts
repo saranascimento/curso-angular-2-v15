3b42b99f059116bd0ad7ae0065212a36df797ec8
Sara Gomes <sarah.gomes80@hotmail.com>
2023-06-06 17:57:48 -0300

    Observable + http-put e delete

--- src/app/services/food-list.service.ts
+++ src/app/services/food-list.service.ts
@@ -34,6 +34,20 @@ export class FoodListService {
     )
   }
 
+  public foodListEdit(value: string, id: number): Observable<FoodList> {
+    return this.http.put<FoodList>(`${this.url}/list-food/${id}`, {nome: value}).pipe(
+      res => res,
+      error => error
+    )
+  }
+
+  public foodListDelete(id: number): Observable<FoodList> {
+    return this.http.delete<FoodList>(`${this.url}/list-food/${id}`).pipe(
+      res => res,
+      error => error
+    )
+  }
+
   public foodListAlert(value: FoodList) {
     return this.emitEvent.emit(value);
   }
